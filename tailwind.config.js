708141ab71ee025ba296184798db5f0171cebdbd
Sergio <harrizmat@mail.ru>
2023-10-10 19:03:46 +0500

    конец первого урока
    добавил разметку хидела

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './src/**/*.html',
+  ],
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myDarkBlue: '#112E42',
+        myBrightBlue: '#3088C2',
+        myDeepBlue: '#24648F',
+        myLightBlue: '#8BBBDC',
+        myGray: '#2D3D47',
+        mydarkGray: '#1D272E',
+
+      }
+    },
   },
   plugins: [],
 }
